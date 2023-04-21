f04c66ae933cce4dab158c406f6565b2bc44246c
syblackwell <syblackwell@anywhichway.com>
2023-04-21 08:02:36 -0700

    Added over-ride support for built-in functions.

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ npm install lmdb-extend
 
 Extends an LMDB root database and any child databases it opens to have the `extensions`.
 
+In order to support over-riding built-in behavior, if the extension is a function and the database already has a function with the same name, the extension will be called with the original function as the first argument already bound to the database instance. It is up to the developer to call the original function at the appropriate time if desired.
+
 # Testing
 
 Testing is conducted using `jest`.
@@ -23,5 +25,7 @@ index.js |     100 |      100 |     100 |     100 |
 
 
 # Release History (reverse chronological order)
+
+2023-04-21 1.1.0 Added over-ride support for built-in functions.
 
 2023-04-20 1.0.0 Initial release
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 function withExtensions(db,extensions) {
     Object.entries(extensions).forEach(([key,value]) => {
-        Object.defineProperty(db,key,{configurable:true,value});
+        if(typeof(db[key])==="function") {
+            const f = db[key].bind(db);
+            Object.defineProperty(db,key,{configurable:true,value:function(...args) {
+                return value(f,...args);
+            }})
+        } else {
+            Object.defineProperty(db,key,{configurable:true,value});
+        }
     })
     const openDB = db.openDB.bind(db);
     db.openDB = function(...args) {
         const db = openDB(...args);
         Object.entries(extensions).forEach(([key,value]) => {
-            Object.defineProperty(db,key,{configurable:true,value});
+            if(typeof(db[key])==="function") {
+                const f = db[key].bind(db);
+                Object.defineProperty(db,key,{configurable:true,value:function(...args) {
+                        return value(f,...args);
+                    }})
+            } else {
+                Object.defineProperty(db,key,{configurable:true,value});
+            }
         })
         return db;
     }
