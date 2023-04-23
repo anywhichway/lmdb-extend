dc86f3b3912caca48cb30681c6b41c44a2037221
syblackwell <syblackwell@anywhichway.com>
2023-04-23 04:59:14 -0700

    Makes environment database available to child databases through the property `envdb`.

--- README.md
+++ README.md
@@ -26,6 +26,8 @@ index.js |     100 |      100 |     100 |     100 |
 
 # Release History (reverse chronological order)
 
+2023-04-23 1.2.0 Makes environment database available to child databases through the property `envdb`.
+
 2023-04-21 1.1.1 Ensure over-ride contexts are bound to the database.
 
 2023-04-21 1.1.0 Added over-ride support for built-in functions.
--- index.js
+++ index.js
@@ -3,26 +3,27 @@ function withExtensions(db,extensions) {
         if(typeof(db[key])==="function") {
             const f = db[key].bind(db);
             Object.defineProperty(db,key,{configurable:true,value:function(...args) {
-                return value.call(this,f,...args);
-            }})
+                    return value.call(this,f,...args);
+                }})
         } else {
             Object.defineProperty(db,key,{configurable:true,value});
         }
     })
     const openDB = db.openDB.bind(db);
     db.openDB = function(...args) {
-        const db = openDB(...args);
+        const childdb = openDB(...args);
+        childdb.envdb = db;
         Object.entries(extensions).forEach(([key,value]) => {
-            if(typeof(db[key])==="function") {
-                const f = db[key].bind(db);
-                Object.defineProperty(db,key,{configurable:true,value:function(...args) {
+            if(typeof(childdb[key])==="function") {
+                const f = childdb[key].bind(childdb);
+                Object.defineProperty(childdb,key,{configurable:true,value:function(...args) {
                         return value.call(this,f,...args);
                     }})
             } else {
-                Object.defineProperty(db,key,{configurable:true,value});
+                Object.defineProperty(childdb,key,{configurable:true,value});
             }
         })
-        return db;
+        return childdb;
     }
     return db;
 }
