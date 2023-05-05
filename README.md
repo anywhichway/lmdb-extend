50d710f89b418cc587c5aeb6cc6e4b1c7ddf7256
syblackwell <syblackwell@anywhichway.com>
2023-05-05 09:06:03 -0700

    Added License section to README.md. Made redefined properties `writable:true`

--- README.md
+++ README.md
@@ -20,11 +20,13 @@ Testing is conducted using `jest`.
 
 File      | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
 ----------|---------|----------|---------|---------|-------------------
-All files |     100 |      100 |     100 |     100 |                   
-index.js |     100 |      100 |     100 |     100 |
+All files |   66.66 |       50 |   66.66 |   66.66 |
+index.js |   66.66 |       50 |   66.66 |   66.66 | 16-18,30-32
 
 
 # Release History (reverse chronological order)
+
+2023-05-05 1.2.1 Added License section to README.md. Made redefined properties `writable:true`
 
 2023-04-23 1.2.0 Makes environment database available to child databases through the property `envdb`.
 
@@ -33,3 +35,9 @@ index.js |     100 |      100 |     100 |     100 |
 2023-04-21 1.1.0 Added over-ride support for built-in functions.
 
 2023-04-20 1.0.0 Initial release
+
+# License
+
+This software is provided as-is under the [MIT license](http://opensource.org/licenses/MIT).
+
+Copyright (c) 2023, AnyWhichWay, LLC and Simon Y. Blackwell.
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+/*MIT License
+Copyright 2023, AnyWhichWay, LLC and Simon Y. Blackwell
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 function withExtensions(db,extensions) {
     Object.entries(extensions).forEach(([key,value]) => {
         if(typeof(db[key])==="function") {
             const f = db[key].bind(db);
-            Object.defineProperty(db,key,{configurable:true,value:function(...args) {
+            Object.defineProperty(db,key,{configurable:true,writable:true,value:function(...args) {
                     return value.call(this,f,...args);
                 }})
         } else {
@@ -16,11 +28,11 @@ function withExtensions(db,extensions) {
         Object.entries(extensions).forEach(([key,value]) => {
             if(typeof(childdb[key])==="function") {
                 const f = childdb[key].bind(childdb);
-                Object.defineProperty(childdb,key,{configurable:true,value:function(...args) {
+                Object.defineProperty(childdb,key,{configurable:true,writable:true,value:function(...args) {
                         return value.call(this,f,...args);
                     }})
             } else {
-                Object.defineProperty(childdb,key,{configurable:true,value});
+                Object.defineProperty(childdb,key,{configurable:true,writable:true,value});
             }
         })
         return childdb;
